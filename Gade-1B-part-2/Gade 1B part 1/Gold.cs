c967b69797519a853aab77ddf0ab7cba4e531f3a
StillAyden <aydenshaw14@gmail.com>
2022-10-11 10:55:36 +0200

    Added 2 Classes
    Item.cs and Gold.cs

---
+++ Gade-1B-part-2/Gade 1B part 1/Gold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gade_1B_part_1
+{
+    abstract internal class Gold: Item
+    {
+        private int goldAmount;
+        public int GoldAmount { get { return goldAmount; } set { goldAmount = value; } }
+
+        public Gold(int X, int Y) : base(X, Y)
+        {
+
+        }
+    }
+}
